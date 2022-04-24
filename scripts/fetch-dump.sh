d9d30ffaa3a0ae44bb19e8467a3641ac7790aad5
jorgeuos <jorge.powers@digitalistgroup.com>
2022-04-24 16:41:41 +0200

    I forgot to update my env

--- scripts/fetch-dump.sh
+++ scripts/fetch-dump.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-mc cp minio/drone/mtmo/matomo-ci.sql.gz ./dumps/matomo-ci.sql.gz
+# shellcheck source=/dev/null
+source ./scripts/check-env.sh
+
+mc cp "${MINIO_BUCKET_PATH}/${DB_DUMP_NAME}.gz" "${IMPORT_DB_DUMP_PATH}/${DB_DUMP_NAME}.gz"
